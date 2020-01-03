51de5f87ed1864b23738af6b971d21262dc2f12f
Shweta Shah <shwetushah123@gmail.com>
2020-01-03 17:06:14 +0530

    new chnages

---
+++ CommentPost/src/main/java/com/SMS/CommentPost/CommentPostApplication.java
@@ -0,0 +1,13 @@
+package com.SMS.CommentPost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentPostApplication.class, args);
+	}
+
+}
---
+++ CommentPost/src/test/java/com/SMS/CommentPost/CommentPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SMS.CommentPost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
